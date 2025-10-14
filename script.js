d9d73f82f1dff2827fd5b76f2e4c581eb404cf61
ThatRook <kafmenn@gmail.com>
2025-10-14 15:42:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const icon = document.querySelector('.bouncing-icon');
+const popup = document.getElementById('discount-popup');
+const closeBtn = document.getElementById('close-popup');
+
+// Show popup on click
+icon.addEventListener('click', () => {
+  popup.style.display = 'block';
+});
+
+// Close popup
+closeBtn.addEventListener('click', () => {
+  popup.style.display = 'none';
+})
